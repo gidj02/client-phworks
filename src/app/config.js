2b93bdfb83584c9488c7c073a866785011e0e62a
gidj02 <ginodejesus02@gmail.com>
2015-08-16 04:27:25 +0800

    Fix Nav

--- src/app/config.js
+++ src/app/config.js
@@ -32,9 +32,9 @@ function config($httpProvider, $stateProvider, $urlRouterProvider, $provide, $oc
             abstract: true,
             url: "/user",
             templateUrl: "assets/views/content.html",
-            // data : {
-            //     requireLogin : true
-            // }
+            data : {
+                requireLogin : true
+            }
         })
         .state('user.dashboard', {
             url: "/home",
@@ -76,5 +76,54 @@ function config($httpProvider, $stateProvider, $urlRouterProvider, $provide, $oc
 }
 angular
     .module('phwork')
-    .config(config);
+    .config(config)
+    .run(function($rootScope, $state, toastr) {
+
+            // $stateChangeStart is fired whenever the state changes. We can use some parameters
+            // such as toState to hook into details about the state as it is changing
+            $rootScope.$on('$stateChangeStart', function(event, toState) {
+
+                // Grab the user from local storage and parse it to an object
+                var user = JSON.parse(localStorage.getItem('user'));            
+                var requireLogin = toState.data.requireLogin;
+                // If there is any user data in local storage then the user is quite
+                // likely authenticated. If their token is expired, or if they are
+                // otherwise not actually authenticated, they will be redirected to
+                // the auth state because of the rejected request anyway
+                
+                if(user) {
+
+
+                    // The user's authenticated state gets flipped to
+                    // true so we can now show parts of the UI that rely
+                    // on the user being logged in
+                    $rootScope.authenticated = true;
+
+                    // Putting the user's data on $rootScope allows
+                    // us to access it anywhere across the app. Here
+                    // we are grabbing what is in local storage
+                    $rootScope.currentUser = user;
+
+                    // If the user is logged in and we hit the auth route we don't need
+                    // to stay there and can send the user to the main state
+                    if(toState.name === "login") {
+
+                        toastr.warning('You\'re currently Logged In.', 'Welcome to Vinteo Inc!');
+                        // Preventing the default behavior allows us to use $state.go
+                        // to change states
+                        event.preventDefault();
+
+                        // go to the "main" state which in our case is users
+                        $state.go('admin.dashboard');
+                    }       
+                }
+                else {
+                    if(requireLogin){
+                        toastr.warning('Please Login First.', 'Welcome to Vinteo Inc!');
+                        event.preventDefault();
+                        $state.go('login');
+                    }
+                }
+            });
+        });
    
